fc1697de5ffd9294405302462077644a428337b3
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-02 14:34:37 +0800

    Remove nix files

--- scripts/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-let
-  inherit (builtins) attrNames readDir;
-
-  scripts = attrNames (readDir ./.);
-
-  mkScript = script: pkgs.writeScriptBin "${script}" (builtins.readFile ./${script}.sh);
-in
-mkScript scripts
