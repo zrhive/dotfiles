7a993c66862f74b44de0c7620baea369bbdf184a
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-25 19:44:02 +0800

    Update

--- scripts/ds-wifi.sh
+++ scripts/ds-wifi.sh
@@ -1,11 +1,9 @@
-#! /usr/bin/env sh
-
-echoco
+#!/usr/bin/env sh
 
 cmd="ip addr; read -p 'Press Enter to exit...'"
 
 case $BUTTON in
     1) notify-send "" ;;
-    3) $TERMINAL -e bash -c "$cmd" ;;
+    3) kitty -e bash -c "$cmd" ;;
 esac
 
