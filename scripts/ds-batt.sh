4f33d88ecc87e27095c45183533ec99958159d50
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-25 23:08:15 +0800

    Update batt

--- scripts/ds-batt.sh
+++ scripts/ds-batt.sh
@@ -4,6 +4,6 @@
 batt_perc=$(upower -b | awk '/percentage/ { print $2 }')
 
 case $BUTTON in
-    1) notify-send "$(batt_perc)" ;;
-    3) kitty -e bash -c "upower -d; read -p 'Press Enter to exit...'" ;;
+    1) notify-send "Battery percentage: $batt_perc" ;;
+    3) kitty -e bash -c "upower -d | less" ;;
 esac
