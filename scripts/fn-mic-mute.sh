acc620f5f0d11f7de61440e7bcb0d9162c177386
zhyie <128297362+zhyie@users.noreply.github.com>
2026-04-26 01:03:54 +0800

    Change Volume to Microphone

--- scripts/fn-mic-mute.sh
+++ scripts/fn-mic-mute.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{if ($3) print $3; else printf "%.0f%%", $2*100}' | xargs -I {} notify-send 'Volume: {}'
+wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk '{if ($3) print $3; else printf "%.0f%%", $2*100}' | xargs -I {} notify-send 'Microphone: {}'
