0d4fb71e8b4580179ecfea269ca85cbf2fbb5d3f
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-03 16:34:44 +0800

    Comment normal float bg

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -2,6 +2,7 @@
 local function background_transparency()
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
