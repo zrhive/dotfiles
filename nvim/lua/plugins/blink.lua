fc2a8450827973409558815cbc03f00ee81d5a2f
zhyie <128297362+zhyie@users.noreply.github.com>
2026-05-16 01:41:27 +0800

    Remove comments

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -36,12 +36,6 @@ return {
       default = { "lsp", "path", "snippets", "buffer" },
     },
 
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    -- fuzzy = { implementation = "prefer_rust_with_warning" }
     fuzzy = { implementation = "prefer_rust" },
   },
   opts_extend = { "sources.default" },
